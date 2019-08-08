d92856bb2adfbf90c47d44f8997e9e581c625759
Jose Armando Mendez <jmendez@simetricaconsulting.com>
2019-08-08 16:04:44 -0400

    Integracion de Unity, Creacion de clase de Dependency Injection

---
+++ CSA.GeneralConfiguration/DependencyInjection/DependencyInjection.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity;
+
+namespace CSA.GeneralConfiguration.DependencyInjection
+{
+    public static class DependencyInjection
+    {
+        public static IUnityContainer unityContainer { get; set; }
+        public static void Run(IUnityContainer container)
+        {
+            #region Services
+
+            #endregion
+            #region DataAccess
+
+            #endregion
+        }
+    }
+}
